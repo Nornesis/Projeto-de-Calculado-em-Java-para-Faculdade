969a7e58e63b13976d5dbedb40508ac955a4e726
Nornesis <160976283+Nornesis@users.noreply.github.com>
2024-12-10 23:18:00 -0300

    Add files via upload

---
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author hadas
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        principal p = new principal();
+        p.setVisible(true);
+    }
+}
